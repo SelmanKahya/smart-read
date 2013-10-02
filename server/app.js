c5ee64e33f52456af755f7f168e1a7efd6af56c2
Selman Kahya <selmanhalid@hotmail.com>
2013-10-01 19:55:32 -0700

    modified app.js - new endpoints

---
+++ server/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var routes = require('./routes');
+var activity = require('./routes/activity');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// home
+app.get('/', routes.index);
+
+// get all activities
+app.get('/activity', activity.readAll);
+
+// get activity by id
+app.get('/activity/:activityID', activity.read);
+
+// create activity
+app.put('/activity', activity.create);
+
+// update activity by id
+app.post('/activity/:activityID', activity.update);
+
+// delete activity by id
+app.delete('/activity/:activityID', activity.delete);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
