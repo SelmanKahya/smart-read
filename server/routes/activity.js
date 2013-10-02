c3e84c52267d0a43749450c52eb52707ab651ed7
Selman Kahya <selmanhalid@hotmail.com>
2013-10-01 19:56:40 -0700

    implemented activity endpoints

---
+++ server/routes/activity.js
@@ -0,0 +1,58 @@
+var config = require('../config.js');;
+var activityModel = require('../models/activity.js');
+var ActivityProvider = require('../providers/activityProvider').ActivityProvider;
+var activityProvider = new ActivityProvider(config.dbConfig);
+
+/*
+ * Activity CRUD operations
+ */
+
+// return activity by id
+exports.read = function(req, res){
+    activityProvider.findById(req.params.activityID, function(error, activity){
+        if(!activity || activity.length == 0)
+            res.send({});
+        else
+            res.send(activity);
+    });
+};
+
+// return all activity
+exports.readAll = function(req, res){
+    activityProvider.findAll(function(error, activities){
+        if(activities.length == 0)
+            res.send({});
+        else
+            res.send(activities);
+    });
+};
+
+// create an activity
+exports.create = function(req, res){
+    var newActivity = new activityModel.Activity(req.body.type, req.body.content);
+
+    activityProvider.save(newActivity, function(error, activity) {
+        res.send(newActivity);
+    });
+};
+
+// update activity
+exports.update = function(req, res){
+
+    if(!req.body.type || !req.body.type)
+        res.send({status: false});
+    else {
+        var activity = new activityModel.Activity(req.body.type, req.body.content);
+
+        activityProvider.update(req.params.activityID, activity, function(error, activity) {
+            res.send(activity);
+        });
+    }
+};
+
+// delete activity
+exports.delete = function(req, res){
+    activityProvider.delete(req.params.activityID, function(error, activity) {
+        res.send({status: true});
+    });
+};
