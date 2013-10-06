f7f74a2424cfc249659f69dd522a2e6525d0a511
Selman Kahya <selmanhalid@hotmail.com>
2013-10-05 22:00:03 -0700

    updated extension popup controller

--- chrome-extension/code/js/popup.js
+++ chrome-extension/code/js/popup.js
@@ -1,34 +1,52 @@
 requirejs.config(requirejsConfig);
 
-requirejs(['jquery', 'util/messagingClient', 'config'],
-function($, client, config) {
-  var popupData = {
-    greeting: config.greeting
-  };
-  client.sendBroadcast({ cmd: 'GetPopupStats' }, function(stats) {
-    switch (stats.counter) {
-      case 1:
-        popupData.counter = 'once';
-        break;
-      case 2:
-        popupData.counter = 'twice';
-        break;
-      default:
-        popupData.counter = '' + stats.counter + ' times';
-        break;
+requirejs(['jquery', 'util/messagingClient', 'config'], function($, client, config) {
+
+    // shows start - stop button based on extension's current tracking status
+    var hideStatusButtons = function(status){
+        if(status){
+            $("#status-start").hide();
+            $("#status-stop").show();
+        }
+        else{
+            $("#status-start").show();
+            $("#status-stop").hide();
+        }
     }
+
+    var popupData = {
+        appName: config.popup.appName,
+        activityLink: config.popup.seeActivityLink
+    };
+
     client.sendBroadcast({
-      cmd: 'GetHtml',
-      args: {
-        template: 'popup',
-        data: popupData
-      }
+        cmd: 'GetHtml',
+        args: {
+            template: 'popup',
+            data: popupData
+        }
     }, function(result) {
-      if (result) {
-        $(function() {
-          $(result).appendTo('body');
-        });
-      }
+        if (result) {
+
+            $(result).appendTo('body');
+
+            var toggleStatus = function(){
+                client.sendBroadcast({ cmd: 'ToggleExtensionStatus' }, function() {
+                    client.sendBroadcast({ cmd: 'GetExtensionStatus' }, function(result) {
+                        // toggle start stop button
+                        hideStatusButtons(result.status);
+                        // reload current page, so that extension can start tracking
+                        chrome.tabs.reload(function(){});
+                    });
+                });
+            }
+
+            // attach events to start - stop tracking buttons
+            client.sendBroadcast({ cmd: 'GetExtensionStatus' }, function(result) {
+                $("#status-start").on( "click", toggleStatus);
+                $("#status-stop").on( "click", toggleStatus);
+                hideStatusButtons(result.status);
+            });
+        }
     });
-  });
 });
