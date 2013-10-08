9f65aff51b7cb56bad50080045e470c888cd3d38
Selman Kahya <selmanhalid@hotmail.com>
2013-10-07 23:31:21 -0700

    changed popup 'see activity' link

--- chrome-extension/code/js/modules/config.js
+++ chrome-extension/code/js/modules/config.js
@@ -2,7 +2,7 @@ define({
   /* Here you define static configuration parameters of your extension. */
   popup: {
       appName: 'Ebook-UAA',
-      seeActivityLink: '#'
+      seeActivityLink: 'http://localhost:3000'
   },
   showTime: 2000 /* ms */
 });
