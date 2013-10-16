95389e479694227e4f7ebfb8ed0345a88d26b76a
Selman Kahya <selmanhalid@hotmail.com>
2013-10-15 19:38:00 -0700

    updated chrome app folder structure

--- chrome-app/test/proxy.php
+++
@@ -1,6 +0,0 @@
-<?php
-// File Name: proxy.php
-if (!isset($_GET['url'])) die();
-$url = urldecode($_GET['url']);
-$url = 'http://' . str_replace('http://', '', $url); // Avoid accessing the file system
-echo file_get_contents($url);
