7e1efd94d1a908e7a57fd037b6c6df165312631e
Selman Kahya <selmanhalid@hotmail.com>
2013-10-15 19:22:00 -0700

    deleted chrome extension
    decided to go with chrome application

--- chrome-extension/code/js/modules/config.js
+++
@@ -1,8 +0,0 @@
-define({
-  /* Here you define static configuration parameters of your extension. */
-  popup: {
-      appName: 'Ebook-UAA',
-      seeActivityLink: 'http://localhost:3000'
-  },
-  showTime: 2000 /* ms */
-});
