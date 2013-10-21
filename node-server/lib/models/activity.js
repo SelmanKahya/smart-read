ebbe2d28c749064459e7e9cf3875bd3d5ae26bf8
Selman Kahya <selmanhalid@hotmail.com>
2013-10-20 18:45:53 -0700

    deleted models -server

--- node-server/lib/models/activity.js
+++
@@ -1,6 +0,0 @@
-// activity entity
-exports.Activity = function (username, type, content) {
-    this.activity_username = username;
-    this.activity_type = type;
-    this.activity_content = content;
-}
