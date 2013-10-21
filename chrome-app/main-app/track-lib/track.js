3be1b6049cff31dfe1b91910d36a14c113374584
Selman Kahya <selmanhalid@hotmail.com>
2013-10-20 21:02:06 -0700

    created track script

---
+++ chrome-app/main-app/track-lib/track.js
@@ -0,0 +1,66 @@
+var ActivityService = new ActivityService();
+
+SMARTREAD = {};
+
+$(document).ready(function(){
+
+    var checkReadiumTimer = setInterval(function(){checkReadium()}, 200);
+    var checkReadium = function(){
+        // if it is loaded, then bind events to elements inside iframe
+        if(window._epubController && window._epub){
+            clearInterval(checkReadiumTimer);
+
+            // INIT book configuration
+            SMARTREAD.book = {
+                id: window._epub.attributes.id,
+                key: window._epub.attributes.key,
+                name: window._epub.attributes.title,
+                numPages: window._epubController.paginator.v.pages.attributes.num_pages,
+                status : {
+                    currentPage: window._epubController.paginator.v.pages.attributes.current_page,
+                    previousPage: null
+                }
+            }
+
+            var pageChanged = function(model, newPage){
+                var oldPage = model._previousAttributes.current_page;
+                SMARTREAD.book.status.currentPage = newPage;
+                SMARTREAD.book.status.previousPage = oldPage;
+
+                // activity_type_id = 3  --->  page_changed
+                ActivityService.newActivity({
+                    activity_type_id: '3',
+                    activity_content: "{old: '" + oldPage + "', new: '" + newPage + "'}",
+                    book_name : '1',
+                    user_id : '1'
+                }, function(){})
+            }
+
+            // attach a function to page change event
+            window._epubController.paginator.v.pages.on("change:current_page", pageChanged, this);
+
+            // TELL SERVER that user started reading a book
+            ActivityService.newActivity({
+                activity_type_id: '1',
+                activity_content: '',
+                book_name : '1',
+                user_id : '1'
+            }, function(){})
+
+        }
+    }
+
+    window.onbeforeunload = function() {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/activity",
+            asnyc: false,
+            data: {
+                activity_type_id: '2',
+                activity_content: '',
+                book_name : '1',
+                user_id : '1'
+            }
+        }).done(function(result) {});
+    }
+});
