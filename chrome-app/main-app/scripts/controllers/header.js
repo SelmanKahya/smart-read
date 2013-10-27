11bb2977751388910fd856cd6b3fa6d180f93f5a
Selman Kahya <selmanhalid@hotmail.com>
2013-10-26 18:35:05 -0700

    updated header controller

--- chrome-app/main-app/scripts/controllers/header.js
+++ chrome-app/main-app/scripts/controllers/header.js
@@ -1,4 +1,8 @@
 // header controller
-mainApp.controller('HeaderCtrl', function ($scope, $location) {
+mainApp.controller('HeaderCtrl', function ($scope, $location, $window) {
     $scope.$location = $location;
+
+    $scope.startReading = function(){
+        $window.location.href = '../views/library.html';
+    }
 });
