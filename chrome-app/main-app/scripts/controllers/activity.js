0373f7ca836c83cf4e6b0fa200a8e2bf4f55bd94
Selman Kahya <selmanhalid@hotmail.com>
2013-10-26 18:34:42 -0700

    deleted activity view & controller

--- chrome-app/main-app/scripts/controllers/activity.js
+++
@@ -1,7 +0,0 @@
-mainApp.controller('ActivityCtrl', function ($scope, $http, sampleFactory) {
-
-    $http({method: 'GET', url: 'http://localhost:3000/activity'}).success(function(data, status, headers, config) {
-        $scope.activities = data;
-    });
-
-});
