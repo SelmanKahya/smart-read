409893fe597014dd4aa159647f4a9d0ba60817ef
Selman Kahya <selmanhalid@hotmail.com>
2013-11-05 00:40:35 -0800

    created register view & controller

---
+++ chrome-app/main-app/scripts/controllers/register.js
@@ -0,0 +1,7 @@
+mainApp.controller('RegisterCtrl', function ($scope, $http) {
+
+    chrome.storage.local.set({'user': null});
+    // chrome.storage.local.set({'user': 'hey'});
+    // console.log('yes')
+
+});
