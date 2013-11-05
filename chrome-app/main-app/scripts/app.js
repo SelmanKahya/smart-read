f750b1355299caef4e828eacb7d7d9a10a972563
Selman Kahya <selmanhalid@hotmail.com>
2013-11-04 23:37:24 -0800

    updated routes

--- chrome-app/main-app/scripts/app.js
+++ chrome-app/main-app/scripts/app.js
@@ -5,24 +5,50 @@ mainApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
+            controller: 'MainCtrl',
+            resolve: { user: mainApp.resolveUser }
         })
         .when('/dashboard', {
             templateUrl: 'views/dashboard.html',
-            controller: 'DashboardCtrl'
+            controller: 'DashboardCtrl',
+            resolve: { user: mainApp.resolveUser }
         })
         .when('/word-lookup', {
             templateUrl: 'views/word-lookup.html',
-            controller: 'WordLookupCtrl'
+            controller: 'WordLookupCtrl',
+            resolve: { user: mainApp.resolveUser }
         })
         .when('/activity', {
             templateUrl: 'views/activity.html',
-            controller: 'ActivityCtrl'
+            controller: 'ActivityCtrl',
+            resolve: { user: mainApp.resolveUser }
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl'
+        })
+        .when('/logout', {
+            templateUrl: 'views/logout.html',
+            controller: 'LoginCtrl'
         })
         .otherwise({
             redirectTo: '/'
         });
 });
+
+mainApp.resolveUser = function($q, $route, $location){
+    var defer = $q.defer();
+    chrome.storage.local.get('user', function (result) {
+        // if not, redirect him to login page
+        if(result.user == null){
+            $location.path('/login');
+            defer.reject();
+            return;
+        }
+        defer.resolve(result.user);
+    });
+    return defer.promise;
+}
 
 mainApp.filter('oneZeroToYesNo', function() {
     return function(input) {
