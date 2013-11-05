5cb8cfa2c2b2ea7d54e335180877426fbccc4c06
Selman Kahya <selmanhalid@hotmail.com>
2013-11-04 23:36:48 -0800

    created login view & controller

---
+++ chrome-app/main-app/scripts/controllers/login.js
@@ -0,0 +1,7 @@
+mainApp.controller('LoginCtrl', function ($scope, $http) {
+
+    chrome.storage.local.set({'user': null});
+    // chrome.storage.local.set({'user': 'hey'});
+    // console.log('yes')
+
+});
