1dd95185c8256287df8268388c7a467ff9bdca0d
Selman K <selmanhalid@hotmail.com>
2013-11-06 23:16:28 -0800

    added to nodejs server - sockjs

--- node-server/app.js
+++ node-server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var sockjs = require('sockjs');
 
 // all environments
 app.use(express.logger('dev'));
@@ -34,14 +35,64 @@ app.get(    '/word-lookup/:id', wordLookup.read);
 app.post(   '/word-lookup/:id', wordLookup.update);
 app.delete( '/word-lookup/:id', wordLookup.delete);
 
+// MEMBERSHIP Routes
+app.post(   '/user/register', user.register);
+app.post(   '/user/login', user.login);
+
 // USER Routes
 app.get(    '/user/:id/activity', user.activity);
 app.get(    '/user/:id/word-lookup', user.lookup);
 app.get(    '/user/:id/word-lookup/books', user.books);
 
 // Server is starting, hold on!
-http.createServer(app).listen(3000, function(){
+var server = http.createServer(app);
+
+server.listen(3000, function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
+// open socket for chat
+var connections = [];
+var chat = sockjs.createServer();
+chat.on('connection', function(conn) {
+
+    connections.push(conn);
+
+    var number = connections.length;
+    var username = 'Guest ' + number;
+
+    conn.write("Welcome to the chat room");
+
+    conn.on('data', function(message) {
+
+        var message = JSON.parse(message);
+
+        if(message.type == 'newUser')  {
+
+            username = message.data.user_first_name + " " + message.data.user_last_name;
+
+            // tell other users that new user entered to room
+            for (var ii=0; ii < connections.length; ii++) {
+                if(connections[ii] != conn)
+                    connections[ii].write(username + " entered to the room");
+            }
+        }
+
+        if(message.type == 'sendingMessage'){
+
+            for (var ii=0; ii < connections.length; ii++) {
+                connections[ii].write(username + " says: " + message.data);
+            }
+
+        }
+    });
+
+    conn.on('close', function() {
+        for (var ii=0; ii < connections.length; ii++) {
+            connections[ii].write(username + " has disconnected");
+        }
+    });
+});
+
+chat.installHandlers(server, {prefix:'/chat'});
 
