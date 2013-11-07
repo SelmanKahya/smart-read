aa3ad6c27538f13956f079483f7382c0ceeb99f6
Selman K <selmanhalid@hotmail.com>
2013-11-07 00:04:13 -0800

    created login, registration - controllers & views

--- chrome-app/main-app/scripts/controllers/login.js
+++ chrome-app/main-app/scripts/controllers/login.js
@@ -1,7 +1,28 @@
-mainApp.controller('LoginCtrl', function ($scope, $http) {
+mainApp.controller('LoginCtrl', function ($scope, $http, $timeout, $location) {
 
     chrome.storage.local.set({'user': null});
-    // chrome.storage.local.set({'user': 'hey'});
-    // console.log('yes')
 
+    $scope.login = function(){
+
+        if($scope.loginForm.$invalid)
+            return;
+
+        $scope.error = null;
+        $scope.processing = true;
+
+        $http.post('http://localhost:3000/user/login', $scope.user).success(function(response, status, headers, config) {
+            if(response.status){
+                var user = response.result;
+                chrome.storage.local.set({'user': user}, function(){
+                    $timeout(function(){
+                        $location.path('/');
+                    });
+                });
+            }
+        }).error(function(response, status, headers, config){
+                $scope.user = null;
+                $scope.error = response.error;
+                $scope.processing = false;
+        });
+    }
 });
--- chrome-app/main-app/scripts/controllers/register.js
+++ chrome-app/main-app/scripts/controllers/register.js
@@ -1,7 +1,27 @@
-mainApp.controller('RegisterCtrl', function ($scope, $http) {
+mainApp.controller('RegisterCtrl', function ($scope, $http, $location, $timeout) {
 
-    chrome.storage.local.set({'user': null});
-    // chrome.storage.local.set({'user': 'hey'});
-    // console.log('yes')
+    $scope.register = function(){
 
+        if($scope.signupForm.$invalid)
+            return;
+
+        $http.post('http://localhost:3000/user/register', $scope.user).success(function(response, status, headers, config) {
+            $scope.processing = true;
+            $scope.error = null;
+
+            if(response.status){
+                var user = response.result;
+                chrome.storage.local.set({'user': user}, function(){
+                    $timeout(function(){
+                        $location.path('/');
+                    });
+                });
+            }
+
+            else {
+                $scope.error = response.error;
+                $scope.processing = false;
+            }
+        });
+    }
 });
