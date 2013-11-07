88a3c1b7f97e56509458160e6ccc318da254e1ae
Selman K <selmanhalid@hotmail.com>
2013-11-07 00:03:46 -0800

    added login & registration routes - server

--- node-server/functions/user.js
+++ node-server/functions/user.js
@@ -1,12 +1,65 @@
 var db = require('../lib/db.js');
 var resultModel = require('../lib/models/result.js');
 
+// register a user
+exports.register = function(req, res){
+
+    // user to be created
+    var user = {
+        user_first_name: req.body.user_first_name,
+        user_last_name: req.body.user_last_name,
+        user_email: req.body.user_email,
+        user_password: req.body.user_password,
+        user_gender: req.body.user_gender,
+        user_created_date: new Date()
+    }
+
+
+    db.execute('INSERT INTO user SET ?', user, function(err, result){
+        if(err)
+            res.send(500, new resultModel.result(false, {}, 'Error while creating the user!'));
+
+        else{
+            user.user_id = result.insertId;
+            res.send(new resultModel.result(true, user));
+        }
+    });
+
+};
+
+// login user
+exports.login = function(req, res){
+
+    // user to be authenticated
+    var user = {
+        user_email: req.body.user_email,
+        user_password: req.body.user_password
+    }
+
+    db.execute('SELECT * FROM user WHERE user_email = ? AND user_password = ?', [user.user_email, user.user_password], function(err, result){
+
+        console.log(result);
+
+        if(err)
+            res.send(500, new resultModel.result(false, {}, ['Error while logging in!]']));
+
+        else{
+            if(result.length != 1){
+                res.send(401, new resultModel.result(false, {}, ['Wrong credentials, please try again!']));
+            }
+
+            else
+                res.send(new resultModel.result(true, result[0]));
+        }
+    });
+};
+
 // return user activities
 exports.activity = function(req, res){
     var user_id = req.params.id;
     db.execute('SELECT * FROM activity WHERE user_id = ?', [user_id], function(err, result){
         if(err)
-            res.send(500, new resultModel.result(false, {}, 'Error while getting word lookup data!'));
+            res.send(500, new resultModel.result(false, {}, ['Error while getting word lookup data!']));
 
         else{
             res.send(new resultModel.result(true, result));
@@ -21,7 +74,7 @@ exports.lookup = function(req, res){
         'word_lookup_created_time, book_name, user_id, COUNT(word_lookup_id) AS word_lookup_count ' +
         'FROM word_lookup WHERE user_id = ? GROUP BY word_lookup_word', [user_id], function(err, result){
         if(err)
-            res.send(500, new resultModel.result(false, {}, 'Error while getting word lookup data!'));
+            res.send(500, new resultModel.result(false, {}, ['Error while getting word lookup data!']));
 
         else{
             res.send(new resultModel.result(true, result));
@@ -34,7 +87,7 @@ exports.books = function(req, res){
     var user_id = req.params.id;
     db.execute('SELECT DISTINCT(book_name) FROM activity WHERE activity_type_id = 1 AND user_id = ?', [user_id], function(err, result){
         if(err)
-            res.send(500, new resultModel.result(false, {}, 'Error while getting books!'));
+            res.send(500, new resultModel.result(false, {}, ['Error while getting books!']));
 
         else{
             res.send(new resultModel.result(true, result));
