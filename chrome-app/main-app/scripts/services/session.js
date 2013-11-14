0c7e7c5ee2b8dbc77fa9953ecde4f89da95bad09
Selman K <selmanhalid@hotmail.com>
2013-11-14 10:21:23 -0800

    implemented session service - check function

--- chrome-app/main-app/scripts/services/session.js
+++ chrome-app/main-app/scripts/services/session.js
@@ -4,6 +4,26 @@ var system = angular.module('service.session', []);
 
 system.factory('session', function($http, $location, $rootScope, $timeout) {
     return {
+        // checks chrome storage if user is already logged in
+        // if yes, assign $rootScope.user and return user
+        // otherwise return null
+        check : function(callback){
+            chrome.storage.local.get('user', function (result) {
+                $timeout(function(){
+
+                    // if yes, assign rootScope.user
+                    if(result.user){
+                        $rootScope.user = result.user;
+                        callback(result.user);
+                    }
+
+                    // if not, return null
+                    else
+                        callback(null);
+                });
+            });
+        },
+        // starts session
         start : function(user, callback){
             chrome.storage.local.set({'user': user}, function(){
                 $timeout(function(){
