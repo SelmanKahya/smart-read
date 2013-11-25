005f3a8def4f9ebf36b91eb8cdd207b4adbd4a4f
Selman K <selmanhalid@hotmail.com>
2013-11-24 20:33:05 -0800

    main app - refactored api service
    customizable api server url

--- chrome-app/main-app/scripts/services/api.js
+++ chrome-app/main-app/scripts/services/api.js
@@ -2,37 +2,38 @@
 
 var api = angular.module('service.api', []);
 
-api.factory('userService', function($http) {
+
+api.factory('userService', function($http, serverOptions) {
     return {
         activity : function(user_id, callback){
-            $http({method: 'GET', url: 'http://localhost:3000/user/' + user_id + '/activity'}).success(function(response, status, headers, config) {
+            $http({method: 'GET', url: serverOptions.getUrl() + '/user/' + user_id + '/activity'}).success(function(response, status, headers, config) {
                 callback(response.result);
             });
         },
         books : function(user_id, callback){
-            $http({method: 'GET', url: 'http://localhost:3000/user/' + user_id + '/word-lookup/books'}).success(function(response, status, headers, config) {
+            $http({method: 'GET', url: serverOptions.getUrl() + '/user/' + user_id + '/word-lookup/books'}).success(function(response, status, headers, config) {
                 callback(response.result);
             });
         },
         wordLookups : function(user_id, callback){
-            $http({method: 'GET', url: 'http://localhost:3000/user/' + user_id + '/word-lookup'}).success(function(response, status, headers, config) {
+            $http({method: 'GET', url: serverOptions.getUrl() + '/user/' + user_id + '/word-lookup'}).success(function(response, status, headers, config) {
                 callback(response.result);
             });
         }
     }
 });
 
-api.factory('activityService', function($http) {
+api.factory('activityService', function($http, serverOptions) {
     return {
 
     }
 });
 
-api.factory('wordLookupService', function($http) {
+api.factory('wordLookupService', function($http, serverOptions) {
     return {
         save: function(word, callback){
             $http({
-                url: "http://localhost:3000/word-lookup/" + word.word_lookup_id,
+                url: serverOptions.getUrl() + "/word-lookup/" + word.word_lookup_id,
                 method: "POST",
                 data: word
             }).success(function(data, status, headers, config) {
@@ -42,10 +43,10 @@ api.factory('wordLookupService', function($http) {
     }
 });
 
-api.factory('member', function($http) {
+api.factory('member', function($http, serverOptions) {
     return {
         login : function(user, callback){
-            $http.post('http://localhost:3000/user/login', user).
+            $http.post(serverOptions.getUrl() + '/user/login', user).
                 success(function(response) {
                     callback(true, response);
                 }).error(function(response){
@@ -53,7 +54,7 @@ api.factory('member', function($http) {
                 });
         },
         register : function(user, callback){
-            $http.post('http://localhost:3000/user/register', user).
+            $http.post(serverOptions.getUrl() + '/user/register', user).
                 success(function(response) {
                     callback(true, response);
                 }).error(function(response){
