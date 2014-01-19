52379ee256c172d16a49c5ab457339a92772fc74
Selman K <selmanhalid@hotmail.com>
2014-01-18 21:23:03 -0800

    refactored logout controller

--- chrome-app/main-app/scripts/controllers/logout.js
+++
@@ -1,6 +0,0 @@
-mainApp.controller('LogoutCtrl', function ($scope, $http, $timeout, $location, member, session) {
-    // first logout the current user even if he already logged in
-    session.end(function(){
-        $location.path('/login');
-    });
-});
