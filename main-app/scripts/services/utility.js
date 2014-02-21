997443e9d76bfc942a4f92e4144f1389acf82963
Selman K <selmanhalid@hotmail.com>
2014-02-20 20:05:58 -0800

    new utiility funcs
    yesno modal box creater, array shuffle func

--- main-app/scripts/services/utility.js
+++ main-app/scripts/services/utility.js
@@ -2,7 +2,7 @@
 
 var system = angular.module('service.utility', []);
 
-system.factory('utilityService', function($http, $location, $rootScope, $timeout) {
+system.factory('utilityService', function($http, $location, $rootScope, $modal, $timeout) {
     return {
         convertToHighChartDatePair : function(dates){
             var result = [];
@@ -13,6 +13,53 @@ system.factory('utilityService', function($http, $location, $rootScope, $timeout
             }
 
             return result;
+        },
+
+        // Shows yes no modal box
+        yesNoModal : function(header, message, callback){
+            var modalInstance = $modal.open({
+                templateUrl: 'views/partial/modal.html',
+                controller: function ($scope, $modalInstance, header, message) {
+                    $scope.header = header;
+                    $scope.message = message;
+                    $scope.ok = function () {
+                        $modalInstance.close();
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                resolve: {
+                    header: function () { return header; },
+                    message: function () { return message; }
+                }
+            });
+
+            modalInstance.result.then(function () {
+                callback(true);
+            }, function () {
+                callback(false);
+            });
+        },
+
+        shuffle: function(array){
+            var shuffle = [];
+
+            // copy array
+            angular.copy(array, shuffle);
+
+            // shuffle elements
+            var i = shuffle.length;
+            while (--i) {
+                var j = Math.floor(Math.random() * (i + 1))
+                var temp = shuffle[i];
+                shuffle[i] = shuffle[j];
+                shuffle[j] = temp;
+            }
+
+            // return mixed array
+            return shuffle;
         }
     }
 });
