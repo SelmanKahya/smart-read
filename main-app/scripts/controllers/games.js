8ecceacd3cd1218a43166f57d4693933ae0f3321
Selman K <selmanhalid@gmail.com>
2014-03-09 17:08:37 -0700

    adding game stats

--- main-app/scripts/controllers/games.js
+++ main-app/scripts/controllers/games.js
@@ -1,5 +1,18 @@
-mainApp.controller('GamesCtrl', function ($scope) {
+mainApp.controller('GamesCtrl', function ($scope, statsService) {
 
+    $scope.stats = {
+        synonyma: '',
+        word_quiz: ''
+    }
 
+    statsService.getPlayed('synonyma', function(response){
+        if(response.status)
+            $scope.stats.synonyma = response.result[0].stats_value;
+    });
+
+    statsService.getPlayed('word_quiz', function(response){
+        if(response.status)
+            $scope.stats.word_quiz = response.result[0].stats_value;
+    });
 
 });
